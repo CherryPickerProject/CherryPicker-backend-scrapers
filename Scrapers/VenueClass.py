ee4c0327836b34cbb67acee733afe79a8ce5829e
ooihuiying <huang.ooihuiying@gmail.com>
2020-07-18 23:39:05 +0800

    This PR formats the time and price scrapped. It also makes major change to the scraper Venuerific so that every object in the final json file represents a room

--- Scrapers/VenueClass.py
+++ Scrapers/VenueClass.py
@@ -2,7 +2,7 @@
 class Venue:
     venue = {}
 
-    def __init__(self, ratings, link, images, title, location, tags, price, pax, description, facilities=[], roomName=[""], promos=""):
+    def __init__(self, ratings, link, images, title, location, tags, price, pax, description, facilities=[], roomNames=[""], promos=""):
         self.venue = {
             "ratings": ratings,
             "link": link,
@@ -14,22 +14,16 @@ class Venue:
             "pax": pax,  # Pax is standing capacity
             "description": description,
             "facilities": facilities,
-            "roomName": roomName,  # Some venues might have more than 1 room
+            "roomNames": roomNames,  # Some venues might have more than 1 room
             "promos": promos,  # Promo string
         }
 
     def getVenue(self):
         return self.venue
 
-# price is a list object of either of the 2 format
-# (1)
+# price is a list object of the format
 # {
 # "dayOfWeek": "",
 # "time": "",
 # "pricing": ""
 # }
-# (2)
-# [{"room": "", "price": ""},
-# {"room": "", "price": ""}
-# {"room": "", "price": ""}
-# ]
